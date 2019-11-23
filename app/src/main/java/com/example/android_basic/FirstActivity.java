3d88163e2c909f531163c0919c568460fdb9aa73
Karan Kumar <kumarkay.ca@gmail.com>
2019-11-22 21:23:13 -0500

    Switch and Toggle Button

--- app/src/main/java/com/example/android_basic/FirstActivity.java
+++ app/src/main/java/com/example/android_basic/FirstActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editText;
     private Button btnGoNext;
+    private ToggleButton tBtn;
+    private TextView txtColor;
+    private Switch aSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +29,42 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
 
         editText=findViewById(R.id.nameEditText);
         btnGoNext=findViewById(R.id.btn);
-
+        tBtn=findViewById(R.id.toggleButton);
+        txtColor=findViewById(R.id.txtColor);
+        aSwitch=findViewById(R.id.switch1);
         btnGoNext.setOnClickListener(this);
+    tBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(tBtn.isChecked())
+                {
+                    txtColor.setTextColor(Color.BLUE);
+                editText.setText(editText.getText().toString().toLowerCase());
+                }
+            else{
+                    txtColor.setTextColor(Color.RED);
+                    editText.setText(editText.getText().toString().toUpperCase());
+                }
+
+            }
+    });
+
+
+    aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(aSwitch.isChecked())
+            {
+                txtColor.setTextColor(Color.BLUE);
+                editText.setText(editText.getText().toString().toLowerCase());
+            }
+            else{
+                txtColor.setTextColor(Color.RED);
+                editText.setText(editText.getText().toString().toUpperCase());
+            }
+
+        }
+    });
 
     }
 
